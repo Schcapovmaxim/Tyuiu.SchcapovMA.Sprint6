2995e39bef851e6eb9b5621f27f85c3dd6d0615e
Максим Щапов <maksimsapov0456@gmail.com>
2024-12-24 10:06:17 +0500

    Task6

---
+++ Tyuiu.SchcapovMA.Sprint6.Task7.V19.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+using System.IO;
+namespace Tyuiu.SchcapovMA.Sprint6.Task7.V19.Lib
+{
+    public class DataService : ISprint6Task7V19
+    {
+        public int[,] GetMatrix(string path)
+        {
+            
+        }
+    }
+}
---
+++ Tyuiu.SchcapovMA.Sprint6.Task7.V19.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.SchcapovMA.Sprint6.Task7.V19.Lib;
+using System.IO;
+namespace Tyuiu.SchcapovMA.Sprint6.Task7.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMatrix()
+        {
+        }
+    }
+}
